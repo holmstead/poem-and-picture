20beef1403ea5b0b5375220b0129a1f37d823d38
Your Name <mat.holmes@outlook.com>
2024-01-25 10:54:05 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -3,18 +3,44 @@ from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
 from kivy.uix.scrollview import ScrollView
-import random, requests, fileinput, os
+import random, requests, fileinput
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 ## scrape the website
-request = requests.get("http://www.poems.com/todays-poem/", headers=None)
+request = requests.get("http://www.poems.com/poem", headers=None)
 soup = BeautifulSoup(request.content, 'html.parser')
 #print(soup.prettify())
 
+# Find all links in the HTML
+all_links = soup.find_all('a')
+
+# Print all the links
+#for link in all_links:
+    #print(link.get('href'))
+
+# Check if there is at least 17 links
+if len(all_links) >= 70:
+    # Print the URL of the 17th link
+    print("link: ", all_links[68].get('href'))
+else:
+    print("Not enough links on the page.")
+
+new_href = all_links[68].get('href')
+print("New href ", new_href)
+request = requests.get(new_href, headers=None)
+soup = BeautifulSoup(request.content, 'html.parser')
+#print(soup.prettify())
+
+# print title
+#print(soup.find_all('p')[0], '\n')
+# print poem
+#print(soup.find_all('h2')[0], '\n')
+# print author
+#print(soup.find_all('p')[1], '\n')
+
 ## get body paragraph **with html tags
 body = (soup.find_all('p')[0])
-
 
 def find_tags(tag):
 	'''locates given html tags in soup'''
@@ -26,7 +52,6 @@ def find_tags(tag):
 
 #find_tags('h2')
 
-
 ## save the scraped poem
 with open("poem.txt", 'w') as outf:
 	#outf.write(str(title))
@@ -34,33 +59,18 @@ with open("poem.txt", 'w') as outf:
 
 ## parse the poem
 def clean_markup(filename):
-    '''Removes html tags from text in text file''' 
-    with fileinput.FileInput(filename, inplace = True, backup ='.bak') as f:
-        for line in f:
-            if('<span class="excerpt_line">' in line):
-                print(line.replace('<span class="excerpt_line">','\n'), end ='')
-            elif('<p>' in line):
-                print(line.replace('<p>','\n'), end ='')
-            elif('&amp;' in line):
-                print(line.replace('&amp;','&'), end ='')
-            elif('<em>' in line):
-                print(line.replace('<em>',''), end ='')
-            elif('</em>' in line):
-                print(line.replace('</em>',''), end ='')
-            elif('</span>' in line):
-                print(line.replace('</span>',''), end ='')
-            elif('</p>' in line):
-                print(line.replace('</p>',''), end ='')
-            else:
-                print(line, end ='')
+    with open(filename, 'r') as file:
+        content = file.read()
+
+    soup = BeautifulSoup(content, 'html.parser')
+    cleaned_text = soup.get_text(separator='\n', strip=True)
+
+    with open(filename, 'w') as file:
+        file.write(cleaned_text)
 
 clean_markup("poem.txt")
-clean_markup("poem.txt")
-clean_markup("poem.txt")
-clean_markup("poem.txt")
-clean_markup("poem.txt")
 
-
+# define the kivy style file
 Builder.load_file('poem.kv')
 
 class MyLayout(BoxLayout):
@@ -87,15 +97,13 @@ class MyLayout(BoxLayout):
 		generates random number and returns a random image path
 		for the kivy file to use as source
 		'''
-		img = str('images/' + str(random.randint(1,len(os.listdir("./images")))) +'.jpg')
+		img = str('images/' + str(random.randint(1,67)) +'.jpg')
 		return img
-
 
 class myApp(App):
     def build(self):
         Window.clearcolor = (1,1,1,1)   # sets background color
         return MyLayout()
-
 
 if __name__ in ('__main__', '__android__'): 
     myApp().run()
